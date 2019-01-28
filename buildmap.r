b0b8e9fc0eeb95d00d28bcefc4d703577c7bdf0f
d-w-moore <dmoore@renci.org>
2019-01-28 21:39:29 +0000

    delete templated part of core.re and changes to set_meta...

--- buildmap.r
+++ buildmap.r
@@ -21,5 +21,65 @@ build_root_lookup(*map)
   }
 }
 
+
+# -- copied in, nominally changed: -- dwm--
+root_in_resc_hier(*h)
+{
+  *r = *h
+  *t = triml(*h,';')
+  if (*t != *h) {
+    msiStrlen(*h, *hl) 
+    msiStrlen(*t, *tl) 
+    msiSubstr(*h,"0",str(int(*hl)-int(*tl)-1),*r)
+  }
+*r
+}
+
+# -- this is for iRODS 4.1:
+trace_hier(*cname,*map,*hier) {
+  msiString2KeyValPair( "", *lin )
+  msiString2KeyValPair( "", *par)
+  msiString2KeyValPair( "", *id2n )
+  msiString2KeyValPair( "", *n2id )
+  msiString2KeyValPair( "", *map )
+  foreach (*y in select RESC_NAME,RESC_ID,RESC_PARENT) {
+     *name = *y.RESC_NAME
+     *parent = *y.RESC_PARENT
+     *idnum = *y.RESC_ID
+     *lin."*name" = "*parent"
+     *id2n."id_*idnum" = *name
+     *n2id."*name" = "*idnum"
+     *par.*name = *parent
+  }
+
+  ## writeLine("stderr", "par = *par")
+  ## writeLine("stderr", "\n----------id2n:")
+  foreach (*z  in *id2n ) {
+    *nam = *id2n."*z"
+    ## writeLine ("stderr", "*z -> *nam ")
+  }
+
+  ## == convert ; par holds id, not name , if vsn >= 42
+
+  ##writeLine("stderr", "----------(par->map):")
+  foreach (*z  in *par ) {
+      *hold = *par.*z
+      *nm = *hold
+      *map.*z = *nm
+  }
+
+   *lst = list()
+   *hier = ""
+   *p ="*cname"
+   while(*p != "") {
+       if (*hier == "") { *hier = *p }
+       else { *hier = *p ++ ";" ++ *hier }
+       *id = *n2id.*p
+       *lst = cons(*id,*lst)
+       *p = *map.*p
+   }
+   *lst
+}
+
 INPUT  null
 OUTPUT ruleExecOut
--- misc/set_meta_on_resc_ambitious.r
+++ misc/set_meta_on_resc_ambitious.r
@@ -1,12 +1,12 @@
 main { test_main }
 
-set_and_check_resc_meta: input string * input string * input string * output string * input string -> integer
+set_and_check_resc_meta: input string * input string * input string * input output string * input string -> integer
 
 test_main {
     *check = ""; *s = 0
     if (*resc_name != '') {
-
-        *s = set_and_check_resc_meta(*resc_name,*k,*v,*check,"*guard")
+        *force_or_check = "*retr"
+        *s = set_and_check_resc_meta(*resc_name,*k,*v,*force_or_check,"*guard") 
 
         if (*s >= 1) { writeLine("stdout","success: [*k] -> [*v] set on resc=[*resc_name].") }
         else if (*s == 0) {
@@ -15,30 +15,37 @@ test_main {
         else if (*s == -1) {
             writeLine("stdout","failure, contention or multiple values on setting metadata")
         }
-        else  {
+        else  { 
             *t = -(*s)
-            writeLine("stdout", "too many kvpairs (*t)\nHint: someone is using " ++
+            writeLine("stdout", "too many kvpairs (*t)\nHint: someone is using " ++ 
                                 "msiAssociateKeyValuePairsToObj(...)")
         }
     }
 }
 
-set_and_check_resc_meta(*resc,*key,*value,*retrieved, *guardstring)
+set_and_check_resc_meta(*resc,*key,*value, *retrieved, *guardstring)
 {
     *vallist = list()
+
+    #=============   Get any previously set value(s) having the *key given
+
     foreach (*x in select META_RESC_ATTR_VALUE,META_RESC_ATTR_NAME,RESC_NAME
                     where META_RESC_ATTR_NAME = '*key' and RESC_NAME = '*resc')
     {
         *vallist = cons(*x.META_RESC_ATTR_VALUE, *vallist)
     }
     *listsize = size(*vallist)
-    #=============
+
+    #=============   Attempt (guarded) metadata set operation 
+
     *success = 1
+
     if (*listsize > 1) {
         *success = (0 - *listsize)
     }
     else {
         *match = false
+        *preexist = ""
         if (*listsize == 1 && *guardstring != "") {
             *preexist = elem(*vallist,0)
             *match = false
@@ -49,14 +56,20 @@ set_and_check_resc_meta(*resc,*key,*value,*retrieved, *guardstring)
             }
         }
         if (*match) {
-            writeLine("stdout","preexist = [] ; guardstring = [*guardstring]")
+            if (*retrieved != "") { # force a waiting condition
+                msiString2KeyValPair("*key=*preexist*retrieved",*kvp)
+                msiSetKeyValuePairsToObj(*kvp,"*resc","-R")
+            }
             *success = 0
         } else {
-            msiString2KeyValPair("*key=*value",*kvp)
+            msiString2KeyValPair("*key=*value",*kvp)     # set the asked-for key/value pair
             msiSetKeyValuePairsToObj(*kvp,"*resc","-R")
         }
     }
-    #=============
+
+    #=============   Check if metadata set operation succeeded
+
+    *retrieved = ""
     if (*success == 1) {
         *tries = 0
         foreach (*x in select META_RESC_ATTR_VALUE,META_RESC_ATTR_NAME,RESC_NAME
@@ -67,13 +80,14 @@ set_and_check_resc_meta(*resc,*key,*value,*retrieved, *guardstring)
                *retrieved = *x.META_RESC_ATTR_VALUE
                if (*value != *retrieved) { *success = -1  }  # contention
             }
-            else {
-               *success = -1 # if (*success > 0) { *success = *tries } # uncomment  to detect multiple matches
+            else {           #  More than one value set....
+               *success = -1 #  We could chg this to return a positive value > 1
+                             #  (Then only a return value of '1' would be a clear success)
             }
         }
     }
-*success  # -- 1 indicates success ; any other return value conveys possible error condition
+*success
 }
 
-input *resc_name=$'pt1', *k=$'aa', *v=$'bb', *guard=$''
+input *resc_name=$'pt1', *k=$'aa', *v=$'bb', *retr=$'', *guard=$''
 output ruleExecOut
