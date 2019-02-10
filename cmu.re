dd31178a44e8146f268a9943b593214c5e0a35f6
d-w-moore <dmoore@renci.org>
2019-02-10 14:53:40 +0000

    move set_meta_*() stuff to cmu.re

--- cmu.re
+++ cmu.re
@@ -103,7 +103,6 @@ is_compound (*resc_name) {
 *found
 }
 
-
         # note -- dwm -- :  for "partial hierarchy" ending in compoundName
 	# feed result of this routine to is_compound()
         # -  if this fails, add ";*" to end, repeat is_compound()
@@ -140,6 +139,46 @@ unset_All_meta_on_compound_resc (*resc_name)
     }
     *x = errorcode(msiRemoveKeyValuePairsFromObj( *kvp, *resc_name, "-R"))
 *x==0;
+}
+
+unset_meta_on_compound_resc( *resc_name, *kvp )
+{
+    *x = errorcode(msiRemoveKeyValuePairsFromObj( *kvp, *resc_name, "-R"))
+    if (*x == 0) { msiString2KeyValPair("",*kvp) }
+}
+
+set_meta_on_compound_resc ( *resc_name, *kvp, *set_value )
+{
+   *match = false
+   *Key = "irods_cache::reserve_resc"
+
+   *rescN = ""
+   foreach (*x in select RESC_NAME, RESC_TYPE_NAME
+            where RESC_NAME = '*resc_name' and RESC_TYPE_NAME = 'compound')
+   {
+       *rescN = *x.RESC_NAME
+   }
+
+   if (*rescN != "") {
+     *kvp."*Key" = "*set_value"
+     msiAssociateKeyValuePairsToObj( *kvp, *rescN, "-R")
+   }
+
+   *equal = ""       # - must be nonzero length for successful set
+   *unequal = list() # - for colliding keys
+   foreach (*x in select RESC_NAME, META_RESC_ATTR_VALUE
+            where META_RESC_ATTR_NAME =  '*Key' and RESC_NAME = '*resc_name' )
+   {
+       if (*set_value == *x.META_RESC_ATTR_VALUE) {
+           *match = true
+       }
+       else {
+           *unequal = cons( *x.META_RESC_ATTR_VALUE, *unequal)
+       }
+   }
+   if (!*match) { msiString2KeyValPair("",*kvp) }
+   # return : bool value  (successful set without collision)
+   *match && size(*unequal) < 1
 }
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
--- set_meta_on_compound_resource.r
+++ set_meta_on_compound_resource.r
@@ -11,60 +11,59 @@ main{
      if (*status) { unset_meta_on_compound_resc( *comp_resc_name,*k) }
 }
 
-unset_All_meta_on_compound_resc( *resc_name )
-{
-    msiString2KeyValPair("",*kvp)
-    foreach (*rm in select META_RESC_ATTR_NAME, META_RESC_ATTR_VALUE
-     where RESC_NAME = '*resc_name' and RESC_TYPE_NAME = 'compound')
-    {
-        *Key = *rm.META_RESC_ATTR_NAME
-        *kvp.*Key = *rm.META_RESC_ATTR_VALUE
-    }
-    *x = errorcode(msiRemoveKeyValuePairsFromObj( *kvp, *resc_name, "-R"))
-*x==0;
-}
-
-unset_meta_on_compound_resc( *resc_name, *kvp ) 
-{
-    *x = errorcode(msiRemoveKeyValuePairsFromObj( *kvp, *resc_name, "-R"))
-    if (*x == 0) { msiString2KeyValPair("",*kvp) }
-}
-
-set_meta_on_compound_resc ( *resc_name, *kvp, *set_value )
-{
-   *match = false
-   *Key = "irods_cache::reserve_resc"
-
-   *rescN = ""
-   foreach (*x in select RESC_NAME, RESC_TYPE_NAME 
-            where RESC_NAME = '*resc_name' and RESC_TYPE_NAME = 'compound') 
-   {
-       *rescN = *x.RESC_NAME
-   }
-
-   if (*rescN != "") {
-     *kvp."*Key" = "*set_value"
-     msiAssociateKeyValuePairsToObj( *kvp, *rescN, "-R")
-   }
-
-   *equal = ""       # - must be nonzero length for successful set
-   *unequal = list() # - for colliding keys
-   foreach (*x in select RESC_NAME, META_RESC_ATTR_VALUE 
-            where META_RESC_ATTR_NAME =  '*Key' and RESC_NAME = '*resc_name' )
-   {
-       if (*set_value == *x.META_RESC_ATTR_VALUE) {
-           *match = true
-       }
-       else {
-           *unequal = cons( *x.META_RESC_ATTR_VALUE, *unequal)
-       }
-   }
-   if (!*match) { msiString2KeyValPair("",*kvp) }
-   # return : bool value  (successful set without collision)
-   *match && size(*unequal) < 1
-}
-
-
+#==================================================
+# unset_All_meta_on_compound_resc( *resc_name )
+# {
+#     msiString2KeyValPair("",*kvp)
+#     foreach (*rm in select META_RESC_ATTR_NAME, META_RESC_ATTR_VALUE
+#      where RESC_NAME = '*resc_name' and RESC_TYPE_NAME = 'compound')
+#     {
+#         *Key = *rm.META_RESC_ATTR_NAME
+#         *kvp.*Key = *rm.META_RESC_ATTR_VALUE
+#     }
+#     *x = errorcode(msiRemoveKeyValuePairsFromObj( *kvp, *resc_name, "-R"))
+# *x==0;
+# }
+#==================================================
+# unset_meta_on_compound_resc( *resc_name, *kvp ) 
+# {
+#     *x = errorcode(msiRemoveKeyValuePairsFromObj( *kvp, *resc_name, "-R"))
+#     if (*x == 0) { msiString2KeyValPair("",*kvp) }
+# }
+# 
+# set_meta_on_compound_resc ( *resc_name, *kvp, *set_value )
+# {
+#    *match = false
+#    *Key = "irods_cache::reserve_resc"
+# 
+#    *rescN = ""
+#    foreach (*x in select RESC_NAME, RESC_TYPE_NAME 
+#             where RESC_NAME = '*resc_name' and RESC_TYPE_NAME = 'compound') 
+#    {
+#        *rescN = *x.RESC_NAME
+#    }
+# 
+#    if (*rescN != "") {
+#      *kvp."*Key" = "*set_value"
+#      msiAssociateKeyValuePairsToObj( *kvp, *rescN, "-R")
+#    }
+# 
+#    *equal = ""       # - must be nonzero length for successful set
+#    *unequal = list() # - for colliding keys
+#    foreach (*x in select RESC_NAME, META_RESC_ATTR_VALUE 
+#             where META_RESC_ATTR_NAME =  '*Key' and RESC_NAME = '*resc_name' )
+#    {
+#        if (*set_value == *x.META_RESC_ATTR_VALUE) {
+#            *match = true
+#        }
+#        else {
+#            *unequal = cons( *x.META_RESC_ATTR_VALUE, *unequal)
+#        }
+#    }
+#    if (!*match) { msiString2KeyValPair("",*kvp) }
+#    # return : bool value  (successful set without collision)
+#    *match && size(*unequal) < 1
+# }
 
 input *comp_resc_name=$'cmp10', *sleep_secs_after_set_meta=$0, *value=$'immediate'
 output ruleExecOut
